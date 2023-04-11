bf663878378572ad736cbd399e59fab24fb45f2a
Nowogórska Laura <242479@edu.p.lodz.pl>
2023-04-11 20:08:25 +0200

    [Added] Starter DaneAPI [Added] Obszar class

---
+++ Etap1/Dane/DaneAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    public abstract class AbstractDaneAPI
+    {
+        //public abstract Obszar obszar { get; }
+        public static AbstractDaneAPI CreateApi()
+        {
+            return new DaneAPI();
+        }
+
+        internal sealed class DaneAPI : AbstractDaneAPI
+        {
+
+        }
+    }
+}
---
+++ Etap1/Dane/Obszar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    internal class Obszar
+    {
+        private int height;
+        private int width;
+        private List<Kula> kule = new List<Kula>();
+
+        public Kula CreateKula(int ballSize)
+        {
+            Random random = new Random();
+            int x, y;
+            x = y = ballSize;
+            bool check = false;
+            while(!check)
+            {
+                x = random.Next(ballSize, width - ballSize);
+                y = random.Next(ballSize, height - ballSize);
+                //We're checking if the new ball isn't inside of another
+                foreach(Kula k in kule)
+                {
+                    double dist = Math.Sqrt(Math.Pow(k.X - x, 2) + Math.Pow(k.Y - y, 2));
+                    if ( dist <= k.R + ballSize)
+                    {
+                        check = false; break;
+                    }             
+                }
+                if (!check) continue;
+                check = true;
+            }
+            Kula kula = new Kula(x, y, ballSize, 1);
+            return kula;
+        }
+
+        public void CreateKulaList(int ballsAmount, int ballsSize)
+        {
+            kule.Clear();
+            for (int i = 0; i < ballsAmount; i++)
+            {
+                Kula kula = CreateKula(ballsSize);
+                kule.Add(kula);
+            }
+        }
+
+        public Obszar(int height, int width, int ballsAmount, int ballsSize)
+        {
+            this.height = height;
+            this.width = width;
+            CreateKulaList(ballsAmount, ballsSize);
+        }
+        public int Height { get { return height; } }
+        public int Width { get { return width; } }
+        public List<Kula> Kule { get { return kule; } }
+    }
+}
