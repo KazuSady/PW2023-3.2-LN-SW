78fe4600f5cd297ff732347ba0f76a0140bdb4a3
242568 Szymon Wydmuch <242456@edu.p.lodz.pl>
2023-04-14 15:52:57 +0200

    Issue with tests

--- Etap1/Dane/Obszar.cs
+++ Etap1/Dane/Obszar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace Dane
     {
         private int height;
         private int width;
-        private bool isEnabled;
+        private bool isRunning = false;
         private List<Kula> kule = new List<Kula>();
 
         public Kula CreateKula(int ballSize)
@@ -58,5 +59,6 @@ namespace Dane
         public int Height { get { return height; } }
         public int Width { get { return width; } }
         public List<Kula> Kule { get { return kule; } }
+        public bool IsRunning { get { return isRunning; } set { isRunning = value; } }
     }
 }
---
+++ Etap1/Testy/ObszarTest.cs
@@ -0,0 +1,23 @@
+﻿using Dane;
+
+namespace Testy
+{
+    [TestClass]
+    public class ObszarTest
+    {
+        Obszar obszar = new Obszar(400, 400, 10, 10);
+        [TestMethod]
+        public void testGetter()
+        {
+            Assert.AreEqual(400, obszar.Height);
+            Assert.AreEqual(400, obszar.Width);
+            Assert.AreEqual(10, obszar.Kule.Count);
+            Assert.AreEqual(10, obszar.Kule.ElementAt(0).R);
+        }
+        public void testSetter()
+        {
+            obszar.IsRunning = true;
+            Assert.AreEqual(true, obszar.IsRunning);
+        }
+    }
+}
