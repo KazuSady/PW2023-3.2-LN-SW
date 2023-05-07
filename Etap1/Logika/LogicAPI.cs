835923eb30abc97041e6b8d8d44fbaac90ac7f41
SzczupakKrolemPontaru <szymonwydmuch1@gmail.com>
2023-05-07 14:25:43 +0200

    [Added] Balls colliding with each other

--- Etap1/Logika/LogicAPI.cs
+++ Etap1/Logika/LogicAPI.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualBasic;
 using System;
 using System.Drawing;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 
@@ -28,7 +29,7 @@ namespace Logika
             private AbstractDataAPI _dataAPI;
             private List<Task> _Tasks = new List<Task>();
             private List<ILogicBall> _logicBalls = new List<ILogicBall>();
-            private SemaphoreSlim _Semaphore = new SemaphoreSlim(1);
+            private readonly object locked = new object();
 
 
             public LogicAPI(AbstractDataAPI abstractDataAPI)
@@ -42,20 +43,32 @@ namespace Logika
             }
             public override void CreateBalls(int ballsAmount, int ballRadius)
             {
+                int x;
+                int y;
                 Random random = new Random();
                 for (int i = 0; i < ballsAmount; i++)
                 {
-                    int x = random.Next(ballRadius, _dataAPI.GetSceneWidth() - ballRadius);
-                    int y = random.Next(ballRadius, _dataAPI.GetSceneHeight() - ballRadius);
-                    _dataAPI.CreateBall(new Point(x, y));
 
+                    x = random.Next(ballRadius, _dataAPI.GetSceneWidth() - ballRadius);
+                    y = random.Next(ballRadius, _dataAPI.GetSceneHeight() - ballRadius);
+
+                    _dataAPI.CreateBall(new Point(x, y));
                     IBall ball = _dataAPI.GetAllBalls().ElementAt(i);
+                    do
+                    {
+                        ball.XMovement = random.Next(-10000, 10000) % 3;
+                        ball.YMovement = random.Next(-10000, 10000) % 3;
+                    } while (ball.XMovement == 0 || ball.YMovement == 0);
+
+
+
                     _logicBalls.Add(ILogicBall.CreateLogicBall(ball.Position.X, ball.Position.Y));
                     ball.PropertyChanged += _logicBalls.ElementAt(i).Update!;
+
                     Task task = new Task(async () =>
                     {
                         startMovement(ball, ballRadius);
-                    }); 
+                    });
                     _Tasks.Add(task);
                 }
             }
@@ -115,25 +128,54 @@ namespace Logika
                 ThreadLocal<Random> random = new ThreadLocal<Random>(() => new Random());
                 while (_dataAPI.IsRunning())
                 {
-                    await _Semaphore.WaitAsync();
-                    ball.XMovement = random.Value.Next(-10000, 10000) % 3;
-                    ball.YMovement = random.Value.Next(-10000, 10000) % 3;
-
-                    if (0 > (ball.Position.X + ball.XMovement) ||
-                        _dataAPI.GetSceneWidth() < (ball.Position.X + ball.XMovement + ballRadius))
+                    ballCollision(ball, ballRadius);
+                    wallColission(ball, ballRadius);
+                    lock (ball)
                     {
-                        ball.XMovement = -ball.XMovement;
+                        ball.MakeMove();
                     }
-                    if (0 > (ball.Position.Y + ball.YMovement) ||
-                        _dataAPI.GetSceneHeight() < (ball.Position.Y + ball.YMovement + ballRadius))
-                    {
-                        ball.YMovement = -ball.YMovement;
-                    }
-                    ball.MakeMove();
-                    _Semaphore.Release();
+                    Task.Delay(50).Wait();
                 }
             }
+            private void wallColission (IBall ball, int ballRadius)
+            {
+                if (0 > (ball.Position.X + ball.XMovement) ||
+                    _dataAPI.GetSceneWidth() < (ball.Position.X + ball.XMovement + ballRadius))
+                {
+                    ball.XMovement = -ball.XMovement;
+                }
+                if (0 > (ball.Position.Y + ball.YMovement) ||
+                    _dataAPI.GetSceneHeight() < (ball.Position.Y + ball.YMovement + ballRadius))
+                {
+                    ball.YMovement = -ball.YMovement;
+                }
+            }
+            private void ballCollision(IBall ball, int ballRadius)
+            {
+                int weight = 1;
+                foreach (IBall otherBall in _dataAPI.GetAllBalls())
+                {
+                    if (otherBall != ball)
+                    {
+                        int xDistance = ball.Position.X - otherBall.Position.X;
+                        int yDistance = ball.Position.Y - otherBall.Position.Y;
+                        double distance = Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2));
 
+                        if ( distance <= (ballRadius) )
+                        {
+                            int newXMovement = (2 * weight * ball.XMovement) / (2 * weight);
+                            ball.XMovement = (2 * weight * otherBall.XMovement) / (2 * weight);
+                            otherBall.XMovement = newXMovement;
+
+                            int newYMovement = (2 * weight * ball.YMovement) / (2 * weight);
+                            ball.YMovement = (2 * weight * otherBall.YMovement) / (2 * weight);
+                            otherBall.YMovement = newYMovement;
+
+                        }
+                    }
+                }
+
+            }
 
         }
     }
